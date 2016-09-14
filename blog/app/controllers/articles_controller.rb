06b8675c9a87b0e75e29f1ec084b49fa1b8718dc
Janis Landwehr <jlandwehr@avarteq.de>
2016-09-14 09:07:10 +0200

    Added delete functionality.
    The delete functionality was missing.
    
    Destroy function added to the articles controller. The functionality is now used in the index.html.erb file to allow the deletion of added entries.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
 	def new
 		@article = Article.new
 	end
-	
+
 	def edit
 		@article = Article.find(params[:id])
 	end
@@ -33,6 +33,12 @@ class ArticlesController < ApplicationController
 	  	else
 	    	render 'edit'
 	  	end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
 	end 
 
 	private

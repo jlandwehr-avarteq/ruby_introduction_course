a976847dc2828768a6e89c035849e7cc0b982473
Janis Landwehr <jlandwehr@avarteq.de>
2016-09-14 08:49:37 +0200

    Fix code and add delete to Article
    > There is duplicate code on new and
    edit view page. Duplicated code is the
    form.
    > Article can not be destroyed
    
    * Remove form from edit and new views
    * Create partial _form
    * Use partial on edit and new views
    * Add `Destroy` action on ArticleController
    * Add a link on index view to trigger the
    destroy action
    * Fix styling errors according to the code review

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
 	def new
 		@article = Article.new
 	end
-	
+
 	def edit
 		@article = Article.find(params[:id])
 	end
@@ -33,11 +33,16 @@ class ArticlesController < ApplicationController
 	  	else
 	    	render 'edit'
 	  	end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
 	end 
 
 	private
 	def article_params
 		params.require(:article).permit(:title, :text)
 	end
-
 end

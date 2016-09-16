f8ada99432c0895dfa1b57fc658d5befe183b1e3
Janis Landwehr <jlandwehr@avarteq.de>
2016-09-16 13:42:54 +0200

    Added EMail confirmation
    > EMail adresses were not validated.
    
    * Added E-Mail confirmation

--- family/app/models/address.rb
+++ family/app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ActiveRecord::Base
+  belongs_to :person 
+  validates :number, presence: true, :numericality => { :greater_than => 0 }
+  validates :street, presence: true
+  validates :zip_code, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
 end
--- family/app/models/person.rb
+++ family/app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ActiveRecord::Base
+  has_one :address, dependent: :destroy
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+  validates :age, presence: true, :numericality => { :greater_than => 0 }
+  validates :email, confirmation: true
+  validates :email_confirmation, presence: true 
 end

b62fb76899cd8fb80cb50d273bd7e0ac41059ca8
Janis Landwehr <jlandwehr@avarteq.de>
2016-09-26 12:21:47 +0200

    Add all untracked files (gca does not track them -.-)

--- family/app/controllers/schools_controller.rb
+++ family/app/controllers/schools_controller.rb
@@ -63,7 +63,6 @@ class SchoolsController < ApplicationController
   end
 
   private
-
     # Use callbacks to share common setup or constraints between actions.
     def set_school
       @school = School.find(params[:id])
